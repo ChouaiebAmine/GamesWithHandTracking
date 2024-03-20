1f9fcf92c434b98ba9999cc501e072c124f68217
ChouaiebAmine <chouaiebmedamine@gmail.com>
2024-03-20 14:31:16 +0100

    Demo

--- HandGame.py
+++ HandGame.py
@@ -1,3 +1,5 @@
+import random
+
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
@@ -8,6 +10,8 @@ import numpy as np
 #Game vars
 cx,cy = 250,250
 color = (64,133,210)
+counter = 0
+number= random.randint(1,5)
 #cam
 cap=cv2.VideoCapture(1)
 cap.set(3,1280)
@@ -33,31 +37,50 @@ while True :
         x1,y1,z1 = lmlist[5]
         x2,y2,z2 = lmlist[17]
 
-
-
         distance= math.sqrt((y2-y1)**2 + (x2-x1)**2)
         A,B,C=coff
         distanceCm= A*distance**2 + B*distance + C
 
-        match fingers.count(1):
+        match number:
             case 1:
-                color = (0, 255, 0)
+
+                if fingers.count(1)== 1:
+                    color = (0, 255, 0)
+                    counter = 1
             case 2:
-                color = (0, 255, 0)
+
+                if fingers.count(1) == 2:
+                    color = (0, 255, 0)
+                    counter = 1
             case 3:
-                color = (0, 255, 0)
+
+                if fingers.count(1) == 3:
+                    counter = 1
             case 4:
-                color = (0, 255, 0)
+                if fingers.count(1) == 4:
+                    color = (0, 255, 0)
+                    counter = 1
             case 5:
-                color = (0, 255, 0)
+
+                if fingers.count(1) == 5:
+                    color = (0, 255, 0)
+                    counter = 1
             case _:
                 color = (64,133,210)
+#counter
+        if counter:
+            counter +=1
+            if counter == 3:
+                color = (64, 133, 210)
+                number= random.randint(1,5)
+                cx = random.randint(100,1100)
+                cy = random.randint(100,600)
         #display distance
-        cvzone.putTextRect(img,f"{int(distanceCm)} cm",(x,y))
-        cv2.rectangle(img,(x,y),(x+w , y + h),(49,111,39),3)
+        cvzone.putTextRect(img,f"{int(fingers.count(1))}Up",(x,y))
+        cv2.rectangle(img,(x,y),(x+w , y + h),(0,255,0),3)
 
     #Button
-    cv2.putText(img,"1",(200,cy),cv2.FONT_HERSHEY_SIMPLEX,1,(15,37,144),2,cv2.LINE_AA)
+    cv2.putText(img,f"{number}",(cx-50,cy),cv2.FONT_HERSHEY_SIMPLEX,1,(15,37,144),2,cv2.LINE_AA)
     cv2.circle(img,(cx,cy),30,color,cv2.FILLED)
     cv2.circle(img, (cx, cy), 10,(255,255,255), cv2.FILLED)
     cv2.circle(img, (cx, cy), 20, (255, 255, 255),2)
