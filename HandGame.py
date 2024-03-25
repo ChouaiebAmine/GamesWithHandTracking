ab0aa2c379bae9888e30f3c418a2f627abcf5796
ChouaiebAmine <chouaiebmedamine@gmail.com>
2024-03-25 15:02:34 +0100

    final changes

--- HandGame.py
+++ HandGame.py
@@ -8,14 +8,16 @@ import time
 
 
 #Game vars
-cx,cy = 250,250
+cx,cy,cx_bait,cy_bait = 250,250,500,500
 color = (64,133,210)
 counter = 0
+wrong_counter = 0
 score=0
-timer=0
 total_time=10
-start_time=time.time()
+start_time = time.time()
 number= random.randint(1,5)
+wrong_number = random.randint(1,5)
+wrong_time = random.randint(0,1)
 #cam
 cap=cv2.VideoCapture(1)
 cap.set(3,1280)
@@ -33,8 +35,8 @@ detector = HandDetector(detectionCon=0.8,maxHands=2)
 
 while True :
     success,img = cap.read()
-
-    if timer <= 10:
+    timer = int(total_time- int(time.time() - start_time))
+    if timer >= 0:
         hands, img = detector.findHands(img, draw=False)
         if hands:
             fingers = detector.fingersUp(hands[0])
@@ -50,33 +52,41 @@ while True :
 
             match number:
                 case 1:
-
                     if fingers.count(1)== 1:
                         color = (0, 255, 0)
                         counter = 1
-
+                    elif fingers.count(1) == wrong_number:
+                        wrong_counter = 1
                 case 2:
 
                     if fingers.count(1) == 2:
                         color = (0, 255, 0)
                         counter = 1
+                    elif fingers.count(1) == wrong_number:
+                        wrong_counter = 1
 
                 case 3:
 
                     if fingers.count(1) == 3:
                         color = (0, 255, 0)
                         counter = 1
+                    elif fingers.count(1) == wrong_number:
+                        wrong_counter = 1
 
                 case 4:
                     if fingers.count(1) == 4:
                         color = (0, 255, 0)
                         counter = 1
+                    elif fingers.count(1) == wrong_number:
+                        wrong_counter = 1
 
                 case 5:
 
                     if fingers.count(1) == 5:
                         color = (0, 255, 0)
                         counter = 1
+                    elif fingers.count(1) == wrong_number:
+                        wrong_counter = 1
 
                 case _:
                     color = (64,133,210)
@@ -90,8 +100,35 @@ while True :
                     score+=10
                     color = (64, 133, 210)
                     number= random.randint(1,5)
-                    cx = random.randint(100,1100)
-                    cy = random.randint(100,600)
+                    cx = random.randint(100, 1100)
+                    cy = random.randint(100, 600)
+
+                    wrong_number=random.randint(1,5)
+                    while number == wrong_number:
+                        wrong_number=random.randint(1,5)
+                    wrong_time = random.randint(0,1)
+                    if wrong_time == 1:
+                        cx_bait = random.randint(100, 1100)
+                        cy_bait = random.randint(100, 600)
+                    else:
+                        wrong_number = -2
+            if wrong_counter:
+                wrong_counter += 1
+                if wrong_counter == 3:
+                    score -=10
+                    number = random.randint(1, 5)
+                    cx = random.randint(100, 1100)
+                    cy = random.randint(100, 600)
+
+                    wrong_number = random.randint(1, 5)
+                    while number == wrong_number:
+                        wrong_number = random.randint(1, 5)
+                    wrong_time = random.randint(0, 1)
+                    if wrong_time == 1:
+                        cx_bait = random.randint(100, 1100)
+                        cy_bait = random.randint(100, 600)
+                    else:
+                        wrong_number = -2
 
         #Button
         cv2.putText(img,f"{number}",(cx-50,cy),cv2.FONT_HERSHEY_SIMPLEX,1,(15,37,144),2,cv2.LINE_AA)
@@ -102,8 +139,16 @@ while True :
         cv2.imshow("Image",img)
         cv2.waitKey(1)
 
+        #Bait Button
+        if wrong_time == 1:
+            cv2.circle(img, (cx_bait, cy_bait), 30, (0,0,255), cv2.FILLED)
+            cv2.putText(img, f"{wrong_number}", (cx_bait - 50, cy_bait), cv2.FONT_HERSHEY_SIMPLEX, 1, (15, 37, 144), 2, cv2.LINE_AA)
+            cv2.circle(img, (cx_bait, cy_bait), 10, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (cx_bait, cy_bait), 30, (0, 0, 0), 2)
+            cv2.circle(img, (cx_bait, cy_bait), 20, (255, 255, 255), 2)
+
+
         #HUD
-        timer= abs(int(total_time - (time.time() - start_time)))
         cvzone.putTextRect(img ,f'Timer : {timer}',(1000,75),scale=2,colorR=(0,0,255))
         cvzone.putTextRect(img, f'Score:{score}', (100, 75), scale=2,colorR=(0,0,255))
     else:
@@ -117,11 +162,12 @@ while True :
     #Restart the Game
     if key == ord('r'):
         counter = 0
+        wrong_counter = 0
         score = 0
-        total_time = 0
-        timer=0
+        total_time = 10
+        timer=10
         start_time = time.time()
-        current_time=0
+
     #Quit the Game
     elif key == ord('q'):
         cv2.destroyAllWindows()
